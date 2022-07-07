bbf44863e28faa27d5d986eb14f1d061c60436d7
Josh F <josh@vector35.com>
2022-07-06 21:42:03 -0400

    Add segment info to widget

--- ui/memorymapview.h
+++ ui/memorymapview.h
@@ -5,6 +5,8 @@
 #include <QtWidgets/QDialog>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QTableWidget>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QPushButton>
 
 #include "dockhandler.h"
 #include "render.h"
@@ -21,14 +23,39 @@ public:
 };
 
 
+class BINARYNINJAUIAPI SegmentDialog : public QDialog
+{
+
+	QPushButton* m_acceptButton;
+	QPushButton* m_cancelButton;
+	QLineEdit* m_startField;
+	QLineEdit* m_lengthField;
+	QLineEdit* m_dataOffsetField;
+	QLineEdit* m_dataLengthField;
+	QCheckBox* m_flagRead;
+	QCheckBox* m_flagWrite;
+	QCheckBox* m_flagExec;
+
+	BinaryViewRef m_data;
+	SegmentRef m_segment;
+
+	void Submit();
+public:
+	SegmentDialog(QWidget* parent, BinaryViewRef data, SegmentRef segment = nullptr);
+};
+
+
 class BINARYNINJAUIAPI MemoryMapSidebarWidget : public SidebarWidget
 {
 	Q_OBJECT
 
-	QTableWidget* m_table;
+	QTableWidget* m_sectionTable;
+	QTableWidget* m_segmentTable;
 	QWidget* m_header;
 	BinaryViewRef m_data;
 
+	void showSegmentContextMenu(const QPoint& point);
+	void showSectionContextMenu(const QPoint& point);
 	void updateInfo();
 
   public:
